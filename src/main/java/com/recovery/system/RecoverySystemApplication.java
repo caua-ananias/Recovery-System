70d2d93fa1fb4f094f3f762506bd6d8203052155
Caua Ananias <caua.ananias2004@gmail.com>
2025-10-29 20:37:52 -0400

    :tada: Commit inicial

---
+++ src/main/java/com/recovery/system/RecoverySystemApplication.java
@@ -0,0 +1,13 @@
+package com.recovery.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecoverySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecoverySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recovery/system/RecoverySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recovery.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecoverySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
